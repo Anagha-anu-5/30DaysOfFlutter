ff4418d3da72e7dfbebf7f6e4b3f82951857806e
Anagha Bagul <anaghabagul54@gmail.com>
2022-06-30 04:49:42 +0530

    Day 5 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,63 +1,61 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-
+ 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter username", labelText: "Username"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      // ignore: avoid_print
-                      print("pressed by anagha");
-                    },
-                    style: TextButton.styleFrom(),
-                    child: Text("Login"))
-              ],
+            const SizedBox(
+              height: 20.0,
             ),
-          )
-        ],
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                      child: Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
